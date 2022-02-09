58fbea18b9cb2aa34c9da7602971e5985c27ea70
Hsuan Shih <aguozili@gmail.com>
2022-02-09 13:00:26 -0800

    C# Day 1 Assignment 1

---
+++ 02UnderstandingTypes/02UnderstandingTypes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02UnderstandingTypes
+{
+    public class Class1
+    {
+        public void CenturyConvert(uint a)
+        {
+
+            uint years = a * 100;
+            uint days = years * 365;
+            uint hours = days * 24;
+            uint minutes = hours * 60;
+            uint seconds = minutes * 60;
+            uint milliseconds = seconds * 1000;
+            uint microseconds = milliseconds * 1000;
+            uint nanoseconds = microseconds * 1000;
+            Console.WriteLine($"{a} centuries = {years} years = {days} days = {hours }hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+
+        }
+    }
+}
+
