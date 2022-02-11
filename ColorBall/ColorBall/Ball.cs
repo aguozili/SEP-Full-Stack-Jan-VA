19d6e893d173664c0a72a9515bceb491e67018bf
Hsuan Shih <aguozili@gmail.com>
2022-02-10 20:15:08 -0800

    C# Day 2 Assignment - Color Ball

---
+++ ColorBall/ColorBall/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorBall
+{
+    public class Ball
+    {
+        public Ball(byte red, byte green, byte blue)
+        {
+            BallColor = new Color(red, green, blue);
+        }
+
+        public Ball(byte red, byte green, byte blue, byte alpha)
+        {
+            BallColor = new Color(red, green, blue, alpha);
+        }
+
+        public Color BallColor;
+
+        public int Size { get; set; }
+
+        public int ThrowCount = 0;
+
+        public void Pop()
+        {
+            Size = 0;
+        }
+
+        public void Throw()
+        {
+            if (Size > 0) 
+            { 
+                ThrowCount++;
+            }
+        }
+
+
+
+
+        
+
+    }
+}
---
+++ ColorBall/ColorBall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ColorBall;
+
+Ball ball = new Ball(33,44,55,5);
+ball.BallColor.GrayScale();
+ball.Size = 14;
+Console.WriteLine("The size is " + ball.Size);
+ball.Throw();
+ball.Throw();
+ball.Throw();
+Console.WriteLine("The throw count is " + ball.ThrowCount);
+Console.WriteLine("Let's pop the ball and throw two times");
+ball.Pop();
+ball.Throw();
+ball.Throw();
+Console.WriteLine("After poping, the throw count is still " + ball.ThrowCount);
+
+
+
+
