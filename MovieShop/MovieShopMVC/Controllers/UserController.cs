dc69992499e3f6a30c519883094e068af5a3f171
Hsuan Shih <aguozili@gmail.com>
2022-03-15 12:03:44 -0700

    Cast Details: moviecard

--- MovieShop/MovieShopMVC/Controllers/UserController.cs
+++ MovieShop/MovieShopMVC/Controllers/UserController.cs
@@ -7,8 +7,38 @@ namespace MovieShopMVC.Controllers
         [HttpGet]
         public async Task<IActionResult> Purchases()
         {
+
+            //wether the uesr is loged in
+            //get the user id 
+            // send the user id to database to get all the movies user purchased.
+
+
+            //cookie based authentication (browser)
             return View();
         }
 
-    }
+        [HttpGet]
+        public async Task<IActionResult> Favorites()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Reviews()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuyMovie()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> FavoriteMovies()
+        {
+            return View();
+        }   
+
+}
 }
